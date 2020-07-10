751c9c0cc5536a0b1678165f241b352178a1b4b6
marjia-anee <marjiazaman@gmail.com>
2020-07-11 00:43:56 +0600

    starting js

---
+++ hello.js
@@ -0,0 +1,9 @@
+console.log('hello');
+console.log('hello');
+console.log('hello');
+var b ='smoothie';
+console.log(b);
+var someNumber = 45;
+console.log(someNumber);
+
+prompt('what is your age');
