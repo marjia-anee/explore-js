39c1f582fecda64e88c922f430a0fffc59bb0288
marjia-anee <marjiazaman@gmail.com>
2020-07-13 11:28:05 +0600

    js files

--- hello.js
+++ hello.js
@@ -1,9 +1,25 @@
-console.log('hello');
-console.log('hello');
-console.log('hello');
-var b ='smoothie';
-console.log(b);
-var someNumber = 45;
-console.log(someNumber);
-
-prompt('what is your age');
+var friendsAge =[15,17,14,16]
+console.log (friendsAge[1])
+friendsAge[0] = 21;
+console.log(friendsAge)
+var aneeAge = friendsAge[3]
+console.log(aneeAge)
+var position = friendsAge.indexOf(14);
+console.log(position)
+var position = friendsAge.indexOf(387);
+console.log(position)
+friendsAge.push(15);
+console.log(friendsAge)
+friendsAge.shift();
+console.log(friendsAge)
+friendsAge.unshift();
+console.log(friendsAge)
+friendsAge.unshift(123);
+console.log(friendsAge)
+friendsAge.pop();
+console.log(friendsAge)
+var tealine = ['bala','mala','lala','pala','fala','tala'];
+console.log(tealine)
+var part = tealine.slice(2,5);
+console.log(part)
+console.log(tealine)
